ab46665adbefe0116802f8e4bfce8ac21a07ae6a
Huiliang Liu <huliu@pivotal.io>
2019-06-17 16:31:10 +0800

    wip: break into functions

--- concourse/scripts/compile_gpdb_oss.bash
+++ concourse/scripts/compile_gpdb_oss.bash
@@ -6,62 +6,109 @@ set -x
 # TODO set these in task yaml
 ORCA_TAG="v3.48.0"
 
-echo "Downloading greenplum-db/gporca@${ORCA_TAG}"
-mkdir orca_src
-wget --quiet --output-document=- "https://github.com/greenplum-db/gporca/archive/${ORCA_TAG}.tar.gz" \
-    | tar xzf - --strip-components=1 --directory=orca_src
+fetch_orca_src () {
+    local orca_tag="${1}"
 
-echo "Building Xerces-C"
-# TODO this works when OUTPUT_DIR is a relative path but fails if an absolute path
-# TODO this does not work when the output dir is outside the current dir
-OUTPUT_DIR="$(mktemp -d --tmpdir=.)"
-mkdir -p xerces_patch/concourse
-cp -r orca_src/concourse/xerces-c xerces_patch/concourse
-cp -r orca_src/patches/ xerces_patch
-/usr/bin/python xerces_patch/concourse/xerces-c/build_xerces.py --output_dir=${OUTPUT_DIR}
-rm -rf build
+    echo "Downloading greenplum-db/gporca@${orca_tag}"
+    mkdir orca_src
+    wget --quiet --output-document=- "https://github.com/greenplum-db/gporca/archive/${orca_tag}.tar.gz" \
+        | tar xzf - --strip-components=1 --directory=orca_src
+}
 
-echo "Building orca"
-orca_src/concourse/build_and_test.py --build_type=RelWithDebInfo --output_dir=${OUTPUT_DIR} --skiptests
+build_xerces () {
+    local output_dir="${1}"
 
-echo "Installing python"
-tar xzf python-tarball/python-*.tar.gz -C /opt --strip-components=2
-# TODO this is gross
-# this is where the src/Makefile.global expects python
-export LD_LIBRARY_PATH="/opt/python-2.7.12/lib"
-export PATH="/opt/python-2.7.12/bin:${PATH}"
+    echo "Building Xerces-C"
+    # TODO this works when OUTPUT_DIR is a relative path but fails if an absolute path
+    # TODO this does not work when the output dir is outside the current dir
+    mkdir -p xerces_patch/concourse
+    cp -r orca_src/concourse/xerces-c xerces_patch/concourse
+    cp -r orca_src/patches/ xerces_patch
+    /usr/bin/python xerces_patch/concourse/xerces-c/build_xerces.py --output_dir=${output_dir}
+    rm -rf build
+}
 
-# TODO set with includes
-INCLUDE_DIR="$(readlink -f ${OUTPUT_DIR})/include"
-LIB_DIR="$(readlink -f ${OUTPUT_DIR})/lib"
-INSTALL_DIR="/usr/local/greenplum-db-oss"
-pushd gpdb_src
-    CC="gcc" CFLAGS="-O3 -fargument-noalias-global -fno-omit-frame-pointer -g" \
-        ./configure \
-            --with-includes="${INCLUDE_DIR}" \
-            --with-libraries="${LIB_DIR}" \
-            --enable-orca \
-            --with-zstd \
-            --with-gssapi \
-            --with-libxml \
-            --with-perl \
-            --with-python \
-            --with-openssl \
-            --with-pam \
-            --with-ldap \
-            --prefix="${INSTALL_DIR}" \
-            --mandir="${INSTALL_DIR}/man"
-    make -j
-    make install
-popd
+build_orca() {
+    local output_dir="${1}"
+    echo "Building orca"
+    orca_src/concourse/build_and_test.py --build_type=RelWithDebInfo --output_dir=${output_dir} --skiptests
+}
 
-echo "Including libxerces-c in greenplum package"
-cp -a ${LIB_DIR}/libxerces-c*.so ${INSTALL_DIR}/lib
+install_python() {
+    echo "Installing python"
+    tar xzf python-tarball/python-*.tar.gz -C /opt --strip-components=2
+}
 
-# Create the python directory to flag to build scripts that python has been handled
-mkdir -p ${INSTALL_DIR}/ext/python
-echo "Copying python from /opt/python-2.7.12 into ${INSTALL_DIR}/ext/python..."
-cp -a /opt/python-2.7.12/* ${INSTALL_DIR}/ext/python
+build_gpdb() {
+    local build_dir
+    build_dir="$(readlink -f "${1}")"
 
-mkdir -p /usr/local/greenplum-db-oss/etc
-mkdir -p /usr/local/greenplum-db-oss/include
+    local greenplum_install_dir="${2}"
+
+    # TODO this is gross
+    # this is where the src/Makefile.global expects python
+    export LD_LIBRARY_PATH="/opt/python-2.7.12/lib"
+    export PATH="/opt/python-2.7.12/bin:${PATH}"
+
+    local include_dir="${build_dir}/include"
+    local lib_dir="${build_dir}/lib"
+
+    pushd gpdb_src
+        CC="gcc" CFLAGS="-O3 -fargument-noalias-global -fno-omit-frame-pointer -g" \
+            ./configure \
+                --with-includes="${include_dir}" \
+                --with-libraries="${lib_dir}" \
+                --enable-orca \
+                --with-zstd \
+                --with-gssapi \
+                --with-libxml \
+                --with-perl \
+                --with-python \
+                --with-openssl \
+                --with-pam \
+                --with-ldap \
+                --prefix="${greenplum_install_dir}" \
+                --mandir="${greenplum_install_dir}/man"
+        make -j
+        make install
+    popd
+
+
+    mkdir -p "${greenplum_install_dir}/etc"
+    mkdir -p "${greenplum_install_dir}/include"
+}
+
+include_xerces() {
+    local build_dir="${1}"
+    local greenplum_install_dir="${2}"
+
+    echo "Including libxerces-c in greenplum package"
+    cp -a ${build_dir}/lib/libxerces-c*.so ${greenplum_install_dir}/lib
+}
+
+include_python() {
+    local greenplum_install_dir="${1}"
+
+    # Create the python directory to flag to build scripts that python has been handled
+    mkdir -p ${greenplum_install_dir}/ext/python
+    echo "Copying python from /opt/python-2.7.12 into ${greenplum_install_dir}/ext/python..."
+    cp -a /opt/python-2.7.12/* ${greenplum_install_dir}/ext/python
+}
+
+_main() {
+    fetch_orca_src "${ORCA_TAG}"
+
+    local build_dir="$(mktemp -d --tmpdir=.)"
+    build_xerces "${build_dir}"
+    build_orca "${build_dir}"
+
+    install_python
+
+    local greenplum_install_dir="/usr/local/greenplum-db-oss"
+    build_gpdb "${build_dir}" "${greenplum_install_dir}"
+
+    include_xerces "${build_dir}" "${greenplum_install_dir}"
+    include_python "${greenplum_install_dir}"
+}
+
+_main "${@}"
